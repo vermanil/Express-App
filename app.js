704954bd45133b6e6906a64a658d971ca708cb9f
vermanil <anilrajverma1996@gmail.com>
2017-09-29 18:07:31 +0530

    Added Es6 javascript syntax

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+import express from 'express';
+import path from 'path';
+// var favicon = require('serve-favicon');
+import logger from 'morgan';
+// var cookieParser = require('cookie-parser');
+import { json, urlencoded } from 'body-parser';
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -17,10 +17,10 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(json());
+app.use(urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
--- dist/app.js
+++ dist/app.js
@@ -1,28 +1,40 @@
 'use strict';
 
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var _express = require('express');
 
+var _express2 = _interopRequireDefault(_express);
+
+var _path = require('path');
+
+var _path2 = _interopRequireDefault(_path);
+
+var _morgan = require('morgan');
+
+var _morgan2 = _interopRequireDefault(_morgan);
+
+var _bodyParser = require('body-parser');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// var favicon = require('serve-favicon');
 var index = require('./routes/index');
+// var cookieParser = require('cookie-parser');
+
 var users = require('./routes/users');
 
-var app = express();
+var app = (0, _express2.default)();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', _path2.default.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use((0, _morgan2.default)('dev'));
+app.use((0, _bodyParser.json)());
+app.use((0, _bodyParser.urlencoded)({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
